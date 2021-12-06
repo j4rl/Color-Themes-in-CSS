e458d6ad9c80ed7920161d47af64744b7efd631e
j4rl <charlie.jarl@gmail.com>
2021-12-06 11:22:28 +0100

    t

--- index.php
+++ index.php
@@ -24,18 +24,14 @@
         $htext="";
         $ord="";
     }
-    if(isset($_POST["datum"])){
-        $datum=$_POST["datum"];
-    }else{
-        $datum="";
-    }
+
 ?>
     <form action="index.php" method="POST">
         <input type="text" name="ord" placeholder="Skriv något...">
         <input type="hidden" name="temp" value="<?=$htext?>">
         <input type="submit" name="btn" value="Skicka">
     </form>
-    <h2><?=$datum?></h2>
+
     <h1><?=$htext?></h1>
 
 </body>
