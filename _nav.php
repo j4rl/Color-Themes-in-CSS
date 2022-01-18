3e31993d95e7179bebcdea7efdfb74e4d625c8de
j4rl <charlie.jarl@gmail.com>
2022-01-18 10:17:21 +0100

    Dreidel

--- _nav.php
+++ _nav.php
@@ -2,5 +2,5 @@
         <a href="index.php">Home</a>
         <a href="about.php">About</a>
         <a href="contact.php">Contact</a>
-       <a href="login.php">Login</a>
+        <a href="login.php">Login</a>
 
